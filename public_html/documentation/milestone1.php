c221115b026f26445298d324221618091093b892
Dante Conley <danteconley@icloud.com>
2017-01-22 20:42:06 -0700

    Persona & Use Case

--- public_html/documentation/milestone1.php
+++ public_html/documentation/milestone1.php
@@ -18,4 +18,37 @@
 	<strong>Goal:</strong>The main goal of my site is to provide an easy way for people to contact me thus hopefully
 	growing my professional network of both collaborators and employers.<br><br>
 
+	<header>
+		<strong>Persona</strong>
+	</header>
+
+	<ul>
+		<li><strong>Name:</strong>Lester Buchannan</li>
+		<li><strong>Age:</strong>early to late 40’s (it’s hard to tell he’s going bald)</li>
+		<li><strong>Profession:</strong>: Lester owns a small carpet and curtain cleaning business he is looking for
+		someone to spice up his very basic and boring website. I may be a candidate.</li>
+		<li><strong>Technology:</strong>2012 17” MacBook Pro, and an older flip phone with no internet access.</li>
+		<li><strong>Attitudes and Behaviors:</strong>Lester doesn’t care about bells and whistles, has had the same phone
+		for 8 years and aside from having to change out the battery it still works great. And he likes that. His computer
+		is older but still works fine as well, so unless something gives out he wont be updating his tech any time soon.</li>
+		<li><strong>Frustrations and Needs:</strong>Even though he doesn’t heavily use technology, he is aware that it is
+		a very powerful marketing tool and he wants to update his websites information and make it more aesthetically
+		pleasing. Right his website hasn’t been updated since its creation 5 years ago. He is looking for somebody to make
+		it look more organized and to update the contact information. He doesn’t want to pay to fortune but is still
+		]willing to spend a pretty penny if he feels the website is created to his expectations.</li>
+		<li><strong>Goals:</strong> Lester is hoping, by visiting my website, to find the right candidate to update his
+		business page. He would also like to get an email to go with his domain name so he can drop the @gmail and make
+		his business look more professional with an @LestersCleanerCarpets domain. My site needs to be visually appealing
+		and straight forward enough that even someone like Lester can easily navigate.</li>
+	</ul>
+
+	<header>
+		<strong>Use Case:</strong>
+	</header>
+	<p>
+		I responded to an ad Lester had on Craigslist with an email. Lester reads my resume that I attached to the email
+		and decides I could possibly be a good fit for the job. To learn a bit more about me and to see the skills I
+		bragged about in my email, he decides to check out my website. Pleased with what he sees he plans to call me
+		Monday on a phone number I have listed under the contact information I have on the page.
+	</p>
 </body>
