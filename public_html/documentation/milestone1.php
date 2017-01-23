2f659e6b9c2b8cabef5754682c87069ec2cacbd4
Dante Conley <danteconley@icloud.com>
2017-01-22 20:17:13 -0700

    Purpose, Audience, Goal

---
+++ public_html/documentation/milestone1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<title>Dante C. PWP</title>title>
+	</head>
+</html>
+
+<body>
+
+	<header>
+		<strong>Purpose, Audience, & Goal</strong>
+	</header>
+
+	<strong>Purpose:</strong>A website that will double as a resume to display my various skills and talents,and provide
+	a time line of work experience<br>
+	<strong>Audience:</strong>A professional audience hopefully consisting of future employers.<br>
+	<strong>Goal:</strong>The main goal of my site is to provide an easy way for people to contact me thus hopefully
+	growing my professional network of both collaborators and employers.<br><br>
+
+</body>
