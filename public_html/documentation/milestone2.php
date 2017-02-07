f15af302f6a3700accd6cb346c9fd3ac978ab230
Dante Conley <danteconley@icloud.com>
2017-02-06 22:07:27 -0700

    milestone2

---
+++ public_html/documentation/milestone2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<title>PWP Content Strategy</title>
+
+		<meta charset="UTF-8">
+		<meta name="viewport"
+				content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	</head>
+
+	<body>
+
+		<header>
+			<strong>Content Strategy</strong>
+		</header>
+
+		<p>
+			On this webpage I would like to display different forms of art I have created. The top of the page would be a
+			bio, and a picture of me. After that would come a “Sight” section, with pictures of original visual art. After
+			the Sight section, would come the “Sound” section, this section would contain a widget linking to my
+			SoundCloud. At the bottom of the page will be my contact information so if visitors like what they see they
+			can get ahold of me. Overall the page will be pretty simple, but visually appealing.
+		</p>
+
+		<img src="images/Trill Wax.jpg" width="350" height="350" alt="TriLLwax"><br><br>
+
+		<img src="images/Trill Wax.jpg" width="350" height="350" alt="TriLLwax"><br><br>
+
+	</body>
+
+</html>
