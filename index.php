a774c674b76961cdb1813024c08895338fe8ee35
Dante Conley <danteconley@icloud.com>
2017-02-26 20:33:31 -0700

    Working on layout.

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+
+		<title>
+			Dante. D Conley
+		</title>
+
+		<meta charset="UTF-8">
+		<meta name="viewport"
+				content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+		<style>
+
+			body {
+				background-color: black;
+			}
+
+			h1 {
+				background-color: #000000;
+				color: #FFFFFF;
+				margin: 25px;
+				letter-spacing: 4px;
+				word-spacing: 2px;
+				visibility: visible;
+			}
+
+			p {
+				background-color: #000000;
+				color: #FFFFFF;
+				margin: 35px;
+				letter-spacing: 4px;
+				word-spacing: 2px;
+				visibility: visible;
+				text-indent: 50px;
+			}
+
+		</style>
+
+	</head>
+
+	<body>
+
+		<div class="container">
+
+			<h1>
+				Dante Conley
+			</h1>
+
+			<p>
+				Wha, me irresponsible ?! All I wanted you to do was to hand me a screwdriver, Morty! Well I don't like your
+				unemployed genes in my grandchildren, Jerry. Listen, Morty, I hate to break it to you but what people call
+				love is just a chemical reaction that compels animals to breed. A price for everything.
+			</p>
+
+			<p>
+				You're not gonna believe this, because it usually never happens, but I made a mistake. Nothing to read into
+				there! I don't like it here Morty. I can't abide bureaucracy. I don't like being told where to go and what
+				to do. I consider it a violation. Did you get those seeds all the way up your butt? Don't even trip about
+				your pants, dawg. We got an extra pair right here.
+			</p>
+
+			<p>
+				There is no god, in your face! One dot muthafucka! You're not gonna believe this, because it usually never
+				happens, but I made a mistake. Yo! What up my glip glops! Are you kidding? I'm hoping I can get to both of
+				them, Rick!
+			</p>
+
+			<h1>
+				Sights
+			</h1>
+
+			<h1>
+				Sounds
+			</h1>
+
+			<h1>
+				Contact
+			</h1>
+
+
+
+		</div>
+
+	</body>
+
+
+</html>
