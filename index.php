dfc1caf5d6591293d0b88d52d7c6001ee7d40dfb
Dante Conley <danteconley@icloud.com>
2017-02-26 21:14:52 -0700

    added contact and sound

--- index.php
+++ index.php
@@ -13,7 +13,6 @@
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 
 		<style>
-
 			body {
 				background-color: black;
 			}
@@ -71,6 +70,10 @@
 			<h1>
 				Sights
 			</h1>
+
+			<h4>
+				<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fdigitaldesert.events%2Fvideos%2F754172264747754%2F&show_text=0&width=560" width="100%" height="250" style="border:none;overflow:hidden" scrolling="no" frameborder="no" allowTransparency="true" allowFullScreen="true"></iframe>
+			</h4>
 			<!--Add Flickity-->
 
 			<h1>
@@ -79,7 +82,11 @@
 			</h1>
 
 			<h4>
-				<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/179394166&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
+				<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/179394166&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false">Warm My Bones</iframe>
+			</h4>
+
+			<h4>
+				<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/90678528&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false">Doin' Real Bad</iframe>
 			</h4>
 
 			<h1>
@@ -91,11 +98,6 @@
 			skype: contact@danteconley.com<br><br>
 			phone: 5O5.2O6.97O3<br>
 			</h4>
-
-
-
-
-
 
 		</div>
 
