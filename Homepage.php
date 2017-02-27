a1bb7a578c6010b4e4f0127490b72cd68cbf37b9
Dante Conley <danteconley@icloud.com>
2017-02-26 19:08:38 -0700

    started <Head>

---
+++ Homepage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+
+		<title>
+			Dante. D Conley
+		</title>
+
+		<meta charset="UTF-8">
+		<meta name="viewport"
+				content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+		<style>
+			h1, div, p {
+				background-color: black;
+				color: white;
+			}
+		</style>
+
+	</head>
+
+
+</html>
--- public_html/documentation/milestone1.php
+++ public_html/documentation/milestone1.php
@@ -19,7 +19,7 @@
 	<strong>Purpose:</strong>A website that will double as a resume to display my various skills and talents,and provide
 	a time line of work experience<br>
 	<strong>Audience:</strong>A professional audience hopefully consisting of future employers.<br>
-	<strong>Goal:</strong>The main goal of my site is to provide an easy way for people to contact me thus hopefully
+	<strong>Goal:</strong>The main goal of my site is to provide an easy way for people to contact me thus
 	growing my professional network of both collaborators and employers.
 </p>
 
