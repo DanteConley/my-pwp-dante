e08546a6e5ef6922c189bf2cd9dac538fb0f1f0c
Dante Conley <danteconley@icloud.com>
2017-02-28 15:41:22 -0700

    added contact and sound

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<head>
 
 		<title>
-			Dante. D Conley
+			Dante D. Conley
 		</title>
 
 		<meta charset="UTF-8">
@@ -12,37 +12,11 @@
 				content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-		<style>
-			body {
-				background-color: black;
-			}
-
-			h1, h4 {
-				background-color: #000000;
-				color: #FFFFFF;
-				margin: 25px;
-				letter-spacing: 4px;
-				word-spacing: 2px;
-				visibility: visible;
-			}
-
-			p {
-				background-color: #000000;
-				color: #FFFFFF;
-				margin: 35px;
-				letter-spacing: 4px;
-				word-spacing: 2px;
-				visibility: visible;
-				text-indent: 50px;
-			}
-
-		</style>
+		<link rel="stylesheet" href="public_html/style.css">
 
 	</head>
 
 	<body>
-
-		<div class="container">
 
 			<h1>
 				Dante Conley
@@ -71,35 +45,43 @@
 				Sights
 			</h1>
 
-			<h4>
-				<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fdigitaldesert.events%2Fvideos%2F754172264747754%2F&show_text=0&width=560" width="100%" height="250" style="border:none;overflow:hidden" scrolling="no" frameborder="no" allowTransparency="true" allowFullScreen="true"></iframe>
-			</h4>
-			<!--Add Flickity-->
+		<div class="container embedded">
+			<iframe
+				src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fdigitaldesert.events%2Fvideos%2F754172264747754%2F&show_text=0&width=560"
+				width="100%" height="100%" style="border:none;overflow:hidden" scrolling="no" frameborder="no"
+				allowTransparency="true" allowFullScreen="true">Meow Wolf DigiDesert
+			</iframe>
+		</div>
 
 			<h1>
 				Sounds
-				<iframe allowtransparency="true" scrolling="no" frameborder="no" src="https://w.soundcloud.com/icon/?url=http%3A%2F%2Fsoundcloud.com%2Fsticky_d&color=white_transparent&size=32" style="width: 32px; height: 32px;"></iframe>
+				<iframe allowtransparency="true" scrolling="no" frameborder="no"
+						  src="https://w.soundcloud.com/icon/?url=http%3A%2F%2Fsoundcloud.com%2Fsticky_d&color=white_transparent&size=32"
+						  style="width: 32px; height: 32px;">SoundCloud Link
+				</iframe>
 			</h1>
 
-			<h4>
-				<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/179394166&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false">Warm My Bones</iframe>
-			</h4>
+		<div class="container embedded">
+			<iframe width="100%" height="166" scrolling="no" frameborder="no"
+					  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/179394166&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false">
+				Warm My Bones
+			</iframe><br><br><br>
 
-			<h4>
-				<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/90678528&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false">Doin' Real Bad</iframe>
-			</h4>
+			<iframe width="100%" height="166" scrolling="no" frameborder="no"
+					  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/90678528&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false">
+				Doin' Real Bad
+			</iframe>
+		</div>
 
 			<h1>
 				Contact
 			</h1>
 
-			<h4>
-			email: contact@danteconley.com<br><br>
-			skype: contact@danteconley.com<br><br>
-			phone: 5O5.2O6.97O3<br>
-			</h4>
+			<p>email: contact@danteconley.com<br><br>
+			<p>skype: contact@danteconley.com<br><br>
+			<p>phone: 5O5.2O6.97O3<br>
+			</p>
 
-		</div>
 
 	</body>
 
