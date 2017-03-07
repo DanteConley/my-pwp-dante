72eee95adf51bad75252fd1af98df083e2028c85
Dante Conley <danteconley@icloud.com>
2017-03-06 21:50:47 -0700

    made video responsive

--- public_html/index.php
+++ public_html/index.php
@@ -7,24 +7,31 @@
 			Dante D. Conley
 		</title>
 
-		<meta charset="utf-8" />
+		<meta charset="utf-8"/>
 		<meta name="viewport"
 				content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 
 		<!-- Bootstrap Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+				crossorigin="anonymous"/>
 
 		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
+				crossorigin="anonymous"/>
 
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 
 		<!-- jQuery Form, Validate, Additional Methods -->
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/jquery.validate.min.js" type="text/javascript"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/additional-methods.min.js" type="text/javascript"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/jquery.validate.min.js"
+				  type="text/javascript"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/additional-methods.min.js"
+				  type="text/javascript"></script>
 
 		<!-- Custom Form Validator -->
 		<script src="js/form-validate.js" type="text/javascript"></script>
@@ -33,7 +40,9 @@
 		<script src='https://www.google.com/recaptcha/api.js'></script>
 
 		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
 
 		<link rel="stylesheet" href="css/style.css">
 
@@ -41,40 +50,41 @@
 
 	<body>
 
-			<h1>
-				Dante Conley
-			</h1>
+		<div class="container">
+		<h1>
+			Dante Conley
+		</h1>
 
-			<p>
-				Wha, me irresponsible ?! All I wanted you to do was to hand me a screwdriver, Morty! Well I don't like your
-				unemployed genes in my grandchildren, Jerry. Listen, Morty, I hate to break it to you but what people call
-				love is just a chemical reaction that compels animals to breed. A price for everything.
-			</p>
+		<p>
+			Wha, me irresponsible ?! All I wanted you to do was to hand me a screwdriver, Morty! Well I don't like your
+			unemployed genes in my grandchildren, Jerry. Listen, Morty, I hate to break it to you but what people call
+			love is just a chemical reaction that compels animals to breed. A price for everything.
+		</p>
 
-			<p>
-				You're not gonna believe this, because it usually never happens, but I made a mistake. Nothing to read into
-				there! I don't like it here Morty. I can't abide bureaucracy. I don't like being told where to go and what
-				to do. I consider it a violation. Did you get those seeds all the way up your butt? Don't even trip about
-				your pants, dawg. We got an extra pair right here.
-			</p>
+		<p>
+			You're not gonna believe this, because it usually never happens, but I made a mistake. Nothing to read into
+			there! I don't like it here Morty. I can't abide bureaucracy. I don't like being told where to go and what
+			to do. I consider it a violation. Did you get those seeds all the way up your butt? Don't even trip about
+			your pants, dawg. We got an extra pair right here.
+		</p>
 
-			<p>
-				There is no god, in your face! One dot muthafucka! You're not gonna believe this, because it usually never
-				happens, but I made a mistake. Yo! What up my glip glops! Are you kidding? I'm hoping I can get to both of
-				them, Rick!
-			</p>
-
-			<h1>
-				Sights
-			</h1>
-
-		<div class="container embedded">
-			<iframe
-				src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fdigitaldesert.events%2Fvideos%2F754172264747754%2F&show_text=0&width=560"
-				width="100%" height="100%" style="border:none;overflow:hidden" scrolling="no" frameborder="no"
-				allowTransparency="true" allowFullScreen="true">Meow Wolf DigiDesert
-			</iframe>
+		<p>
+			There is no god, in your face! One dot muthafucka! You're not gonna believe this, because it usually never
+			happens, but I made a mistake. Yo! What up my glip glops! Are you kidding? I'm hoping I can get to both of
+			them, Rick!
+		</p>
 		</div>
+
+
+		<div class="container">
+			<div class="row">
+		<h1>
+			Sights
+		</h1>
+
+				<div class="embed-responsive embed-responsive-16by9">
+					<iframe class="embed-responsive-item" src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fdigitaldesert.events%2Fvideos%2F754172264747754%2F&show_text=0&width=560"></iframe>
+				</div>
 
 			<h1>
 				Sounds
@@ -84,74 +94,77 @@
 				</iframe>
 			</h1>
 
-		<div class="container embedded">
-			<iframe width="100%" height="166" scrolling="no" frameborder="no"
-					  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/179394166&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false">
-				Warm My Bones
-			</iframe><br><br><br>
 
-			<iframe width="100%" height="166" scrolling="no" frameborder="no"
-					  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/90678528&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false">
-				Doin' Real Bad
-			</iframe>
+				<iframe width="100%" height="166" scrolling="no" frameborder="no"
+						  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/179394166&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false">
+					Warm My Bones
+				</iframe>
+				<br><br><br>
+
+				<iframe width="100%" height="166" scrolling="no" frameborder="no"
+						  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/90678528&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false">
+					Doin' Real Bad
+				</iframe>
+			</div>
 		</div>
 
-
+		<div class="container">
 			<h1>
-				Contact
+				Collaborate
 			</h1>
 
-
-		<!--Begin Contact Form-->
-		<form id="contact-form" action="php/mailer.php" method="post">
-			<div class="form-group">
-				<label for="name">Name <span class="text-danger">*</span></label>
-				<div class="input-group">
-					<div class="input-group-addon">
-						<i class="fa fa-user" aria-hidden="true"></i>
+			<!--Begin Contact Form-->
+			<form id="contact-form" action="php/mailer.php" method="post">
+				<div class="form-group">
+					<label for="name">Name</label>
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="fa fa-user" aria-hidden="true"></i>
+						</div>
+						<input type="text" class="form-control" id="name" name="name" placeholder="Name">
 					</div>
-					<input type="text" class="form-control" id="name" name="name" placeholder="Name">
 				</div>
-			</div>
-			<div class="form-group">
-				<label for="email">Email <span class="text-danger">*</span></label>
-				<div class="input-group">
-					<div class="input-group-addon">
-						<i class="fa fa-envelope" aria-hidden="true"></i>
+				<div class="form-group">
+					<label for="email">Email</label>
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</div>
+						<input type="email" class="form-control" id="email" name="email" placeholder="Email">
 					</div>
-					<input type="email" class="form-control" id="email" name="email" placeholder="Email">
 				</div>
-			</div>
-			<div class="form-group">
-				<label for="subject">Subject</label>
-				<div class="input-group">
-					<div class="input-group-addon">
-						<i class="fa fa-pencil" aria-hidden="true"></i>
+				<div class="form-group">
+					<label for="subject">Subject</label>
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="fa fa-pencil" aria-hidden="true"></i>
+						</div>
+						<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
 					</div>
-					<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
 				</div>
-			</div>
-			<div class="form-group">
-				<label for="message">Message <span class="text-danger">*</span></label>
-				<div class="input-group">
-					<div class="input-group-addon">
-						<i class="fa fa-comment" aria-hidden="true"></i>
+				<div class="form-group">
+					<label for="message">Message</label>
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="fa fa-comment" aria-hidden="true"></i>
+						</div>
+						<textarea class="form-control" rows="5" id="message" name="message"
+									 placeholder="What should we create?"></textarea>
 					</div>
-					<textarea class="form-control" rows="5" id="message" name="message" placeholder="Message (2000 characters max)"></textarea>
 				</div>
-			</div>
 
-			<!-- reCAPTCHA -->
-			<div class="g-recaptcha" data-sitekey="6LfvXxcUAAAAAPJLSMEi7olCqiCpRgPS8fwFraUY"></div>
+				<!-- reCAPTCHA -->
+				<div class="g-recaptcha" data-sitekey="6LfvXxcUAAAAAPJLSMEi7olCqiCpRgPS8fwFraUY"></div>
 
-			<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
-			<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
-		</form>
+				<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+				<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+			</form>
 
-		<!--empty area for form error/success output-->
-		<div class="row">
-			<div class="col-xs-12">
-				<div id="output-area"></div>
+			<!--empty area for form error/success output-->
+			<div class="row embedded">
+				<div class="col-xs-12">
+					<div id="output-area"></div>
+				</div>
 			</div>
 		</div>
 
